53df31f75d0c31acf617af3efcb76336c8383917
Helena Moreno Chacon <helenaenred@gmail.com>
2014-07-15 09:13:53 +0200

    Added thumbnails to projects section

---
+++ js/my.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){	
+	// var myOptions = {
+	// 	zoom: 16,
+	// 	center: latlng,
+	// 	mapTypeId: google.maps.MapTypeId.ROADMAP
+	// };
+
+
+	// var marker = new google.maps.Marker({
+	// 	position: latlng, 
+	// 	map: map, 
+	// 	title: placeName
+	// });
+});
